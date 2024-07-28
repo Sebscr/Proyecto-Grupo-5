17dfae4b0789bc353496649b79f3e098d1b7a745
wolnic26 <148506891+Wolnic@users.noreply.github.com>
2024-07-28 17:33:08 -0600

    Update Visitantes.java

--- Proyecto/src/proyecto/Visitantes.java
+++ Proyecto/src/proyecto/Visitantes.java
@@ -3,11 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package proyecto;
-
+import javax.swing.JOptionPane;
+import java.util.Date;
 /**
  *
  * @author David
  */
-public class Visitantes {
-    
+public class Visitante {
+    private String nombre;
+    private int edad;
+    private Date fechaIngreso;
+
+    public Visitante(String nombre, int edad, Date fechaIngreso) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public Date getFechaIngreso() {
+        return fechaIngreso;
+    }
 }

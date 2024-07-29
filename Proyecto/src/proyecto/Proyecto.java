05c95b5875f508100ba886d83ccd0b0bfd343e68
wolnic26 <148506891+Wolnic@users.noreply.github.com>
2024-07-28 18:32:33 -0600

    Update Proyecto.java

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
+import javax.swing.JOptionPane;
 /**
  *
  * @author David

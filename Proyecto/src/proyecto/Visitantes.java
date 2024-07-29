f9ed3e7a97b68a2d512d140cb1c11e23bbfad887
wolnic26 <148506891+Wolnic@users.noreply.github.com>
2024-07-28 18:30:57 -0600

    Update Visitantes.java

--- Proyecto/src/proyecto/Visitantes.java
+++ Proyecto/src/proyecto/Visitantes.java
@@ -9,25 +9,46 @@ import javax.swing.JOptionPane;
  * @author David
  */
 public class Visitante {
-    private String nombre;
+    // Atributos privados para almacenar la información de cada visitante
+  private String nombre;
     private int edad;
     private String fechaIngreso;
-
+    
+     // Constructor de atributos a valores predeterminados
+    public Visitante() {
+        this.nombre = "";
+        this.edad = 0;
+        this.fechaIngreso = "";
+    }
+    
+     // Constructor de atributos al crear un objeto Visitante
     public Visitante(String nombre, int edad, String fechaIngreso) {
         this.nombre = nombre;
         this.edad = edad;
         this.fechaIngreso = fechaIngreso;
     }
-
+        // Método para obtener el nombre del visitante
     public String getNombre() {
         return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public int getEdad() {
         return edad;
     }
 
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
     public String getFechaIngreso() {
         return fechaIngreso;
+    }
+
+    public void setFechaIngreso(String fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
     }
 }

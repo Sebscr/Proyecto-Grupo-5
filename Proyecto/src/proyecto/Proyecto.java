f47591f3599a389f29cdb3f659be185fdd3b6c1e
DavidG <72959512+DavidgG10@users.noreply.github.com>
2024-07-30 18:10:19 -0600

    .

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -76,7 +76,7 @@ public class Proyecto {
 
         gestion.consultarHabitats();  
 
-/*
+
        private static void gestionarEventos() {
         Evento[] eventos = new Evento[10];
         int numEventos = 0;
@@ -163,7 +163,7 @@ public class Proyecto {
                     break;
             }
         }
-    }*/
+    }
       int RegistroAlimentacion= Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese la cantidad de registros que desea crear: "));
         Alimentacion[] arrAlimentacion = new Alimentacion[RegistroAlimentacion];
         for (int i=0; i < RegistroAlimentacion; i++){  
